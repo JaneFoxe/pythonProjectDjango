c119c4dcab99ec560fb8e7222674ad41218327d9
JaneFoxe <Kukusiki2204@gmail.com>
2024-03-06 23:53:26 +0300

    добавлены контроллеры

--- catalog/views.py
+++ catalog/views.py
@@ -3,3 +3,11 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'catalog/index.html')
+
+
+def homepage(request):
+    return render(request, 'catalog/home.html')
+
+
+def contactspage(request):
+    return render(request, 'catalog/contacts.html')
