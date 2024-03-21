719626e4aace0e32b6710aecf6cbe88201af5e2e
JaneFoxe <Kukusiki2204@gmail.com>
2024-03-21 16:28:58 +0300

    дз

--- catalog/models.py
+++ catalog/models.py
@@ -2,31 +2,31 @@ from django.db import models
 
 
 class Product(models.Model):
-    name = models.CharField(max_length=100)
-    description = TextField()
-    image = ImageField(upload_to='product/')
-    category = models.models.ForeignKey(Category, on_delete=models.DO_NOTHING)
-    price = models.DecimalField(max_digits=10, decimal_places=2)
-    created_at = models.DateTimeField()
-    updated_at = models.DateTimeField()
+    name = models.CharField(max_length=100, verbose_name='Наименование')
+    description = models.TextField(verbose_name='Описание', blank=True)
+    image = models.ImageField(upload_to='product/', verbose_name='Изображение',null=True, blank=True)
+    category = models.ForeignKey("catalog.Category", on_delete=models.SET_NULL, verbose_name='Категория',null=True, blank=True)
+    price = models.DecimalField(max_digits=10, decimal_places=2, verbose_name='Цена за покупку')
+    created_at = models.DateTimeField(auto_now_add=True,verbose_name='Дата создания (записи в БД)')
+    updated_at = models.DateTimeField(auto_now_add=True,verbose_name='Дата последнего изменения (записи в БД)')
 
     def __str__(self):
         # Строковое отображение объекта
         return f'{self.name} | {self.description}'
 
     class Meta:
-        verbose_name = 'продукт'  # Настройка для наименования одного объекта
-        verbose_name_plural = 'продукты'  # Настройка для наименования набора объектов
+        verbose_name = 'Продукт'  # Настройка для наименования одного объекта
+        verbose_name_plural = 'Продукты'  # Настройка для наименования набора объектов
 
 
 class Category(models.Model):
-    name = models.CharField(max_length=100)
-    description = TextField()
+    name = models.CharField(max_length=100, verbose_name='Наименование')
+    description = models.TextField(verbose_name='Описание')
 
     def __str__(self):
         # Строковое отображение объекта
         return f'{self.name} | {self.description}'
 
     class Meta:
-        verbose_name = 'категория'  # Настройка для наименования одного объекта
-        verbose_name_plural = 'категории'  # Настройка для наименования набора объектов
+        verbose_name = 'Категория'  # Настройка для наименования одного объекта
+        verbose_name_plural = 'Категории'  # Настройка для наименования набора объектов
