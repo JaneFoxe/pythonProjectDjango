9ecbfebece53a5c6c4a5d08021d4e252f6201335
JaneFoxe <Kukusiki2204@gmail.com>
2024-03-26 23:51:43 +0300

    дз

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,8 +1,14 @@
 from django.urls import path, include
 
-from catalog.views import homepage, contactspage
+from catalog.views import homepage, contactspage, product_card
+from catalog.apps import MainappConfig
+
+app_name = MainappConfig.name
+
+
 
 urlpatterns = [
-    path('', homepage),
-    path('contacts/', contactspage)
+    path('', homepage,),
+    path('contacts/', contactspage),
+    path('<int:pk>/', product_card, name='product_card')
 ]
