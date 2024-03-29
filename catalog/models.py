6691d492e9cdd00d5ee214ef04308abe9de9979f
JaneFoxe <Kukusiki2204@gmail.com>
2024-03-29 22:08:11 +0300

    product -> media

--- catalog/models.py
+++ catalog/models.py
@@ -4,7 +4,7 @@ from django.db import models
 class Product(models.Model):
     name = models.CharField(max_length=100, verbose_name='Наименование')
     description = models.TextField(verbose_name='Описание', blank=True)
-    image = models.ImageField(upload_to='product/', verbose_name='Изображение',null=True, blank=True)
+    image = models.ImageField(upload_to='media/', verbose_name='Изображение',null=True, blank=True)
     category = models.ForeignKey("catalog.Category", on_delete=models.SET_NULL, verbose_name='Категория',null=True, blank=True)
     price = models.DecimalField(max_digits=10, decimal_places=2, verbose_name='Цена за покупку')
     created_at = models.DateTimeField(auto_now_add=True,verbose_name='Дата создания (записи в БД)')
