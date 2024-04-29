9ada2bfb4084e88b7fd7c1adfe819e23329f8250
Nastya Foxe <Kukusiki2204@gmail.com>
2024-04-30 00:23:09 +0300

    feat: remove * from .gitignore feat: add  forms.py, migrations and  other

---
+++ catalog/forms.py
@@ -0,0 +1,29 @@
+from django import forms
+
+from catalog.models import Product
+
+
+class ProductForm(forms.ModelForm):
+    class Meta:
+        model = Product
+        fields = '__all__'
+
+    def clean_name(self):
+        cleaned_data = self.cleaned_data['name']
+        bad_words = ['казино', 'криптовалюта', 'крипта', 'биржа', 'дешево', 'бесплатно', 'обман', 'полиция', 'радар']
+
+        for word in bad_words:
+            if word in cleaned_data:
+                raise forms.ValidationError(f'Недопустимое слово: {word}')
+
+        return cleaned_data
+
+    def clean_description(self):
+        cleaned_data = self.cleaned_data['description']
+        bad_words = ['казино', 'криптовалюта', 'крипта', 'биржа', 'дешево', 'бесплатно', 'обман', 'полиция', 'радар']
+
+        for word in bad_words:
+            if word in cleaned_data:
+                raise forms.ValidationError(f'Недопустимое слово: {word}')
+
+        return cleaned_data
