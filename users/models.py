147b1a4bddd16fec7e83d4eaccf642996c926115
Nastya Foxe <Kukusiki2204@gmail.com>
2024-05-03 16:33:45 +0500

    дз6

---
+++ users/models.py
@@ -0,0 +1,16 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+from blog.models import NULL_PARAM
+
+
+class User(AbstractUser):
+    username = None
+    email = models.EmailField(unique=True, verbose_name='Почта')
+
+    avatar = models.ImageField(upload_to="user/", verbose_name="Аватар", **NULL_PARAM)
+    phone = models.CharField(max_length=30, unique=True, verbose_name='Номер телефона', **NULL_PARAM)
+    country = models.CharField(max_length=100, verbose_name="Страна")
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = []
